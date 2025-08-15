030223628b45d0f2fcd92c8bf34a268b975b54ae
Vaishnavi <vaishnavipatil.9333@gmail.com>
2025-08-15 21:46:23 +0530

    using dutch national flag algorithm

---
+++ src/day1.java
@@ -0,0 +1,35 @@
+/*
+Problem : Sort an Array of 0s, 1s, and 2s
+You are given an array arr consisting only of 0s, 1s, and 2s. The task is to sort the array in increasing order in linear time (i.e., O(n)) without using any extra space. This means you need to rearrange the array in-place.
+*/
+public class day1 {
+    public static void sortColors(int[] arr) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 1, 0, 2, 1, 0};
+        sortColors(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
